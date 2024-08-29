83af2af64c2e2b742a8fbfd967a6a78db219b3c0
lovoski <liuwei.contact@qq.com>
2024-08-29 17:15:34 +0800

    metrics interface

--- Engine/Function/Animation/Metrics.cpp
+++ Engine/Function/Animation/Metrics.cpp
@@ -4,7 +4,9 @@ namespace aEngine {
 
 namespace Animation {
 
+void JerkMetrics(Motion &gt, Motion &pred) {}
 
+void SlideMetrics(Motion &gt, Motion &pred) {}
 
 };
 
--- Engine/Function/Animation/Metrics.hpp
+++ Engine/Function/Animation/Metrics.hpp
@@ -1,10 +1,16 @@
 #include "Function/Animation/Motion.hpp"
+#include "Function/Animation/Motion.hpp"
+#include "Function/Math/Math.hpp"
 
 namespace aEngine {
 
 namespace Animation {
 
+// Positional jitter (3rd derivative): smootheness of motion.
+void JerkMetrics(Motion &gt, Motion &pred);
 
+// Foot skating from Mode-Adaptive Neural Networks for Quadruped Motion Control.
+void SlideMetrics(Motion &gt, Motion &pred);
 
 };
 
